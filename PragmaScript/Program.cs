4716958978f65c716b8525220bd22fdd577f9b5f
pragmascript <pragmascript@googlemail.com>
2015-04-09 18:01:58 +0200

    Initial commit

---
+++ PragmaScript/Program.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LLVMSharp;
+using System.Runtime.InteropServices;
+
+namespace PragmaScript
+{
+    class Token
+    {
+        public enum TokenType
+        {
+            WhiteSpace, Let, Var, Fun, Identifier, OpenBracket, CloseBracket, Number, Assignment, Error, Add, Subtract, Multiply, Divide
+        }
+
+
+        public TokenType type { get; private set; }
+        public string text { get; private set; }
+        public string errorMessage { get; private set; }
+        public int lineNumber { get; private set; }
+        public int pos { get; private set; }
+        public int length { get; private set; }
+
+        static Dictionary<string, TokenType> keywords;
+        static Dictionary<string, TokenType> operators;
+
+
+        static Token()
+        {
+            keywords = new Dictionary<string, TokenType>();
+            keywords.Add("let", TokenType.Let);
+            keywords.Add("var", TokenType.Var);
+
+            operators = new Dictionary<string, TokenType>();
+            operators.Add("=", TokenType.Assignment);
+            operators.Add("(", TokenType.OpenBracket);
+            operators.Add(")", TokenType.CloseBracket);
+            operators.Add("+", TokenType.Add);
+            operators.Add("-", TokenType.Subtract);
+            operators.Add("*", TokenType.Multiply);
+            operators.Add("/", TokenType.Divide);
+        }
+
+        public static bool isIdentifierChar(char c)
+        {
+            return char.IsLetter(c) || c == '_';
+        }
+
+        public static bool isKeyword(string identifier)
+        {
+            return keywords.ContainsKey(identifier);
+        }
+
+        public static bool isOperator(char c)
+        {
+            return operators.ContainsKey(c.ToString());
+        }
+
+        public static Token EmitError(Token t, string errorMessage)
+        {
+            t.errorMessage = errorMessage;
+            t.type = TokenType.Error;
+            return t;
+        }
+        public static Token Parse(string line, int pos, int lineNumber)
+        {
+            var t = new Token();
+            t.pos = pos;
+            t.lineNumber = lineNumber;
+            t.length = 0;
+
+            char current = line[pos];
+
+            // first test if char is whitespace
+            if (char.IsWhiteSpace(current))
+            {
+                while (char.IsWhiteSpace(current))
+                {
+                    t.length++;
+                    pos++;
+                    if (pos >= line.Length)
+                        break;
+                    current = line[pos];
+                }
+
+                t.type = TokenType.WhiteSpace;
+                t.text = line.Substring(t.pos, t.length);
+                return t;
+            }
+
+
+            // test if first char is a radix 10 digit
+            if (char.IsDigit(current))
+            {
+                bool containsDecimalSeperator = false;
+                while (char.IsDigit(current) || current == '.')
+                {
+                    // only one decimal seperator is allowed
+                    if (current == '.' && containsDecimalSeperator)
+                    {
+                        t.text = line.Substring(t.pos, t.length);
+                        return EmitError(t, "only one decimal seperator is allowed!");
+                    }
+                    containsDecimalSeperator = current == '.';
+
+                    t.length++;
+                    pos++;
+                    if (pos >= line.Length)
+                        break;
+                    current = line[pos];
+                }
+                t.type = TokenType.Number;
+                t.text = line.Substring(t.pos, t.length);
+                return t;
+            }
+
+            // if a token starts with a leter its either a keyword or an identifier
+            if (char.IsLetter(current))
+            {
+                while (isIdentifierChar(current))
+                {
+                    t.length++;
+                    pos++;
+                    if (pos >= line.Length)
+                        break;
+                    current = line[pos];
+                }
+
+                var identifier = line.Substring(t.pos, t.length);
+                t.text = identifier;
+
+                // check if current identifier is a reserved keyword
+                if (isKeyword(identifier))
+                {
+                    t.type = keywords[identifier];
+                    return t;
+                }
+                else
+                {
+                    t.type = TokenType.Identifier;
+                    return t;
+                }
+            }
+
+            // check if current char is operator
+            if (isOperator(current))
+            {
+                // TOOD: support combined operators of multipel chars ( e.g. +=, == )
+                t.length = 1;
+                t.type = operators[current.ToString()];
+                t.text = line.Substring(t.pos, t.length);
+                return t;
+            }
+
+            t.length = 1;
+            t.text = line.Substring(t.pos, t.length);
+            return EmitError(t, "syntax error!");
+        }
+
+        public override string ToString()
+        {
+            if (type != TokenType.Error)
+            {
+                return string.Format("({0}, {1}, {2}, \"{3}\")", type.ToString(), lineNumber, pos, text);
+            }
+            else
+            {
+                return string.Format("({0}, {1}, {2}, \"{3}\")", "error: " + errorMessage, lineNumber, pos, text);
+            }
+
+        }
+    }
+
+    class Backend
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate int Add(int a, int b);
+        public static void Test()
+        {
+
+            LLVMBool False = new LLVMBool(0);
+            LLVMModuleRef mod = LLVM.ModuleCreateWithName("LLVMSharpIntro");
+
+            LLVMTypeRef[] param_types = { LLVM.Int32Type(), LLVM.Int32Type() };
+            LLVMTypeRef ret_type = LLVM.FunctionType(LLVM.Int32Type(), out param_types[0], 2, False);
+            LLVMValueRef sum = LLVM.AddFunction(mod, "sum", ret_type);
+
+            LLVMBasicBlockRef entry = LLVM.AppendBasicBlock(sum, "entry");
+
+            LLVMBuilderRef builder = LLVM.CreateBuilder();
+            LLVM.PositionBuilderAtEnd(builder, entry);
+            LLVMValueRef tmp = LLVM.BuildAdd(builder, LLVM.GetParam(sum, 0), LLVM.GetParam(sum, 1), "tmp");
+            LLVM.BuildRet(builder, tmp);
+
+            IntPtr error;
+            LLVM.VerifyModule(mod, LLVMVerifierFailureAction.LLVMAbortProcessAction, out error);
+            LLVM.DisposeMessage(error);
+
+            LLVMExecutionEngineRef engine;
+
+            LLVM.LinkInMCJIT();
+            LLVM.InitializeX86Target();
+            LLVM.InitializeX86TargetInfo();
+            LLVM.InitializeX86TargetMC();
+            LLVM.InitializeX86AsmPrinter();
+
+            var platform = Environment.OSVersion.Platform;
+            if (platform == PlatformID.Win32NT) // On Windows, LLVM currently (3.6) does not support PE/COFF
+            {
+                LLVM.SetTarget(mod, Marshal.PtrToStringAnsi(LLVM.GetDefaultTargetTriple()) + "-elf");
+            }
+
+            var options = new LLVMMCJITCompilerOptions();
+            var optionsSize = (4 * sizeof(int)) + IntPtr.Size; // LLVMMCJITCompilerOptions has 4 ints and a pointer
+
+            LLVM.InitializeMCJITCompilerOptions(out options, optionsSize);
+            LLVM.CreateMCJITCompilerForModule(out engine, mod, out options, optionsSize, out error);
+
+            var addMethod = (Add)Marshal.GetDelegateForFunctionPointer(LLVM.GetPointerToGlobal(engine, sum), typeof(Add));
+            int result = addMethod(10, 10);
+
+            Console.WriteLine("Result of sum is: " + result);
+
+            if (LLVM.WriteBitcodeToFile(mod, "sum.bc") != 0)
+            {
+                Console.WriteLine("error writing bitcode to file, skipping");
+            }
+
+            LLVM.DumpModule(mod);
+
+            LLVM.DisposeBuilder(builder);
+            LLVM.DisposeExecutionEngine(engine);
+            Console.ReadKey();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parse("let x = 12.0");
+            parse("let y = x + 5");
+            parse("%%");
+            parse("var z = (2 + 3) * 5.0");
+            Console.ReadLine();
+            Backend.Test();
+            Console.ReadLine();
+        }
+
+
+        static IEnumerable<Token> tokenize(string text)
+        {
+            List<Token> result = new List<Token>();
+
+            var lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                var line = lines[i];
+                var pos = 0;
+                while (pos < line.Length)
+                {
+                    var t = Token.Parse(line, pos, i);
+                    yield return t;
+
+                    // abort on first error token
+                    if (t.type == Token.TokenType.Error)
+                    {
+                        yield break;
+                    }
+                    pos += t.length;
+                }
+            }
+        }
+
+        static void parse(string text)
+        {
+            var tokens = tokenize(text).ToList();
+            Console.WriteLine("input: " + text);
+            Console.WriteLine();
+            Console.WriteLine("tokens: ");
+            int pos = 0;
+            foreach (var t in tokens)
+            {
+                if (t.type != Token.TokenType.WhiteSpace)
+                    Console.WriteLine("{0}: {1}", pos++, t);
+            }
+
+
+        }
+    }
+}
