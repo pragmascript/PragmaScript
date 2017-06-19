e8640609678140b1b29061a28f0ce2ab414adbe9
pragmascript <pragmascript@googlemail.com>
2017-06-19 21:13:29 +0200

    updated publish script

--- src/publish.py
+++ src/publish.py
@@ -1,20 +1,19 @@
-from shutil import copy, copytree, rmtree, make_archive
+from shutil import copy, copytree, rmtree, make_archive, move
 from subprocess import call
 import os
 import time
 
 cwd = os.getcwd()
-print(cwd)
 
 def nj(*paths):
     return os.path.normpath(os.path.join(*paths)) 
-call("dotnet clean PragmaCore.csproj -c release")
-call("dotnet publish PragmaCore.csproj -c release -r win7-x64")
-binPath = nj(cwd, "bin/release/netcoreapp2.0/win7-x64")
+call("dotnet clean -c release")
+call("dotnet publish -c release -r win7-x64")
+binPath = nj(cwd, "bin/Release/netcoreapp2.0/win7-x64/publish")
 publishPath = nj(cwd, "../publish/pragma")
 
 copytree(binPath, nj(publishPath, "bin"))
-copytree(nj(cwd, "bin/external"), nj(publishPath, "bin/external"))
+copytree(nj(cwd, "../publish/current/bin/external"), nj(publishPath, "bin/external"))
 
 def ignore(path, names):
     ignored = []
@@ -24,7 +23,8 @@ def ignore(path, names):
             ignored.append(x)
     return ignored
 
-copytree(nj(cwd, "samples"), nj(publishPath, "samples"), ignore=ignore)
-archive_name = nj(cwd, "publish", time.strftime("pragma_%Y_%m_%d"))
-make_archive(archive_name, "zip", nj(cwd, "publish"), "pragma")
+copytree(nj(cwd, "../publish/current/samples"), nj(publishPath, "samples"), ignore=ignore)
+archive_path = nj(cwd, "../publish", time.strftime("pragma_%Y_%m_%d"))
+make_archive(archive_path, "zip", nj(cwd, "../publish"), "pragma")
+move(archive_path+".zip", nj(cwd, "../publish/releases",))
 rmtree(publishPath)
